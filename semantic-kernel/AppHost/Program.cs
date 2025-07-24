0f8ff99468c2486e8e166f5c294737887723557e
Gian <gianclaudio.gc@gmail.com>
2025-07-24 17:50:48 +0200

    cleaning and add work service

--- semantic-kernel/AppHost/Program.cs
+++ semantic-kernel/AppHost/Program.cs
@@ -8,4 +8,6 @@ builder.AddProject<Projects.Chat>("chat");
 // Add the Customer API
 builder.AddProject<Projects.Customer_API>("customerapi");
 
+builder.AddProject<Projects.WorkerService>("worker");
+
 builder.Build().Run();
